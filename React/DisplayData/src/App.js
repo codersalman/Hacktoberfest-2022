ec782c14fcf82f851a004ed9b53766efa3dc7095
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-10-09 13:34:14 +0530

    my react app code added

---
+++ React/DisplayData/src/App.js
@@ -0,0 +1,13 @@
+import DisplayData from "./DisplayData";
+
+function App() {
+  return (
+       <div className="App">
+      <header className="App-header">
+       <DisplayData/> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
