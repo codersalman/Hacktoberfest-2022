6531f80b341bb1e34e003ccf0f90a064cb81cca5
Dipesh_codes <111185281+bdipesh3045@users.noreply.github.com>
2022-10-15 21:14:54 +0545

    Create BMI.js

---
+++ Javascript/Bmi Calculator/BMI.js
@@ -0,0 +1,24 @@
+function calc()
+{
+    var h,w,bmi;
+    h = parseFloat(document.getElementById("h").value);
+    w = parseFloat(document.getElementById("w").value);
+    bmi=((w)/(h*h)).toFixed(2);
+                if(bmi <= 18.5){
+                document.getElementById("value").innerHTML ="Underweight";
+                }
+                else if(bmi>= 18.5 && bmi<=24.8 ){
+                    document.getElementById("value").innerHTML ="Normal weight";
+
+                }
+                else if(bmi>= 25 && bmi<=29.9 ){
+                    document.getElementById("value").innerHTML ="Normal weight";
+
+                }
+                else{
+                    document.getElementById("value").innerHTML = "Over weight";
+                }
+
+    console.log(bmi);
+    document.getElementById("print").innerHTML = bmi;
+}
