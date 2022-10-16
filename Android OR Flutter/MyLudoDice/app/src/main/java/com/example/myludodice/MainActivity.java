0e3a30a44dddeb4623b51b35fb0ec818f27e56b4
Shubhanshu Jha <shubhanshujha.s19@gmail.com>
2022-10-17 00:12:13 +0530

    Added a Ludo Dice android application project

---
+++ Android OR Flutter/MyLudoDice/app/src/main/java/com/example/myludodice/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myludodice;
+
+import java.util.Random;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageViewDice;
+    private Random rng = new Random();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageViewDice = findViewById(R.id.image_view_dice);
+        imageViewDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rollDice();
+            }
+        });
+    }
+
+    private void rollDice(){
+        int rdNum = rng.nextInt(6) + 1;
+        switch (rdNum){
+            case 1:
+                imageViewDice.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                imageViewDice.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                imageViewDice.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                imageViewDice.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                imageViewDice.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                imageViewDice.setImageResource(R.drawable.dice6);
+                break;
+        }
+    }
+}
