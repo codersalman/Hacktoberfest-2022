19d0b130968622c8f9d7260d631e91be56ed5940
ankitranjan1028 <aankitrn17@gmail.com>
2022-10-18 22:47:50 +0530

    login

---
+++ Javascript/My_login_page/index.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+function register() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+
+}
+function login() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
